aef06b5f880779be9771772cc6b4d10c1b9d0b2c
Rakesh5834 <rakeshyerra2001@gmail.com>
2025-11-04 16:03:00 +0530

    lb code

---
+++ lb.tf
@@ -0,0 +1,12 @@
+resource "vpc" "vpco1" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = " Rakesh vpc"
+    }
+  
+}
+
+resource "aws_subnet" "subnet01" {
+    vpc_id = aws_vpc.vpc01.id
+    cidr_block = "10.1.1.0/24"
+}
